85e1110390e74db9ad03ffb79cb4952c37a7f852
Eric4106 <56258073+Eric4106@users.noreply.github.com>
2019-10-17 07:45:38 -0400

    Add files via upload

--- LiarsDice.java
+++ LiarsDice.java
@@ -19,23 +19,32 @@ public class LiarsDice {
         System.out.println("dice you think their are amoung all of the hands in all of the players' hands.");
         System.out.println("You can only bet a value higher then the previous dice value or any dice value of a higher number.");
         System.out.println("If you think your oponent is lying, then you can call. If you are correct, you win.");
-        System.out.println("To bet, type 'bet', then type the amount of dice, then the type of dice you think are on the board");
+        System.out.println("To bet, type 'bet', then type the amount of dice, then the type of dice you think are on the board.");
         System.out.println("If you think your oponent is a liar, then just type 'call'.");
         System.out.println("");
         System.out.println("You roll your dice,");
         System.out.println("");
-        System.out.println("Your dice read "+ player.dice[0] +", "+ player.dice[1] +", "+ player.dice[2] +", "+ player.dice[3] +", and "+ player.dice[4]);
-        boolean end = false;
-        while (!end) {
-            String commandInput = kb.nextLine();
-            if (commandInput.equals("bet")) {
-                
+        System.out.println("Your dice read "+ player.pDice[0] +", "+ player.pDice[1] +", "+ player.pDice[2] +", "+ player.pDice[3] +", and "+ player.pDice[4]+ ".");
+        System.out.println("");
+        while (!player.end) {
+            player.pBet = kb.nextLine();
+            if (player.pBet.equals("bet")) {
+                System.out.println("How many of the dice number do you want to bet?");
+                player.pAmount = kb.nextInt();
+                System.out.println("What dice number do you want to bet?");
+                player.pNumber = kb.nextInt();
+                player.cThink();
+                if (player.cBet.equals("call")) {
+                    System.out.println("The Computer thinks you're a liar and says 'call'");
+                    player.cCall();
+                }
+                else {
+                    System.out.println("The computer says their are " + player.cAmount + " dice of " + player.cNumber + ".");
+                }
             }
-            else if (commandInput.equals("call")) {
-                
-            }
-            else {
-                System.out.println("That is not a valid function.");
+            else if (player.pBet.equals("call")) {
+                System.out.println("You call the computer's bluff.");
+                player.pCall();
             }
         }
     }
--- Player.java
+++ Player.java
@@ -8,21 +8,115 @@ package liars.dice;
 // @author 710568
 
 public class Player {
-    int[] dice = new int[5];
+    boolean end = false;
+    int pAmount = 0;
+    int pNumber = 0;
+    int cAmount = 0;
+    int cNumber = 0;
+    String pBet = null;
+    String cBet = null;
+    int[] pDice = new int[6];
+    int[] cDice = new int[6];
+    int[] cTotal = new int[6];
+    int[] total = new int[6];
     public Player() {
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = randDie();
+        for (int i = 0; i < pDice.length; i++) {
+            pDice[i] = randDie();
+            if (pDice[i] == 1) {
+                total[0]++;
+            }
+            else if (pDice[i] == 2) {
+                total[1]++;
+            }
+            else if (pDice[i] == 3) {
+                total[2]++;
+            }
+            else if (pDice[i] == 4) {
+                total[3]++;
+            }
+            else if (pDice[i] == 5) {
+                total[4]++;
+            }
+            else {
+                total[5]++;
+            }
+        }
+        for (int i = 0; i < cDice.length; i++) {
+            cDice[i] = randDie();
+            if (cDice[i] == 1) {
+                total[0]++;
+                cTotal[0]++;
+            }
+            else if (cDice[i] == 2) {
+                total[1]++;
+                cTotal[1]++;
+            }
+            else if (cDice[i] == 3) {
+                total[2]++;
+                cTotal[2]++;
+            }
+            else if (cDice[i] == 4) {
+                total[3]++;
+                cTotal[3]++;
+            }
+            else if (cDice[i] == 5) {
+                total[4]++;
+                cTotal[4]++;
+            }
+            else {
+                total[5]++;
+                cTotal[5]++;
+            }
         }
     }
     
-    public void newDice() {
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = randDie();
-        }
-    }
-    
-    public static int randDie() {
+    public int randDie() {
         int rand = (int) ((Math.random() * 6) + 1);
         return rand;
+    }
+    
+    public void cThink() {
+        if (pAmount > cTotal[pNumber - 1] + (int) ((Math.random() * 3) + 1)) {
+            cBet = "call";
+        }
+        else {
+            cBet = "bet";
+            if (pAmount < 3) {
+                cAmount = 3;
+                cNumber = cLargest();
+            }
+        }
+        if (cNumber < pNumber) {
+            cNumber = pNumber + 1;
+        }
+        if (cAmount <= pAmount) {
+            cAmount = pAmount + 1;
+        }
+    }
+    
+    public int cLargest() {
+        int max = 0;
+        for (int i = 1; i < cDice.length; i++) {
+            if (cDice[i] > max) {
+                max = cDice[i];
+            }
+        }
+        return max;
+    }
+    
+    public void pCall() {
+        if (cAmount > total[cNumber - 1]) {
+            System.out.println("You are correct, the computer was lying.");
+            System.out.println("Their were actually only " + total[cNumber - 1] + " total dice of number " + cNumber);
+        }
+        end = true;
+    }
+    
+    public void cCall() {
+        if (pAmount > total[pNumber - 1]) {
+            System.out.println("The computer caught you in your lie, you lost.");
+            System.out.println("Their were actually only " + total[pNumber - 1] + " total dice of number " + pNumber);
+        }
+        end = true;
     }
 }
